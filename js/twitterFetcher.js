9827f54a2d4dff9a08e8ea6a2b04961eedb3c927
Jason Mayes <jasonmayes@users.noreply.github.com>
2016-10-10 11:21:31 -0700

    Update to 16.0.3
    Fixes bug when author does not exist

--- js/twitterFetcher.js
+++ js/twitterFetcher.js
@@ -1,5 +1,5 @@
 /*********************************************************************
-*  #### Twitter Post Fetcher v16.0.2 ####
+*  #### Twitter Post Fetcher v16.0.3 ####
 *  Coded by Jason Mayes 2015. A present to all the developers out there.
 *  www.jasonmayes.com
 *  Please keep this disclaimer with my code if you use it. Thanks. :-)
