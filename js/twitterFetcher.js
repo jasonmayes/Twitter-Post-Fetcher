e9e519500831ef6ef207abea892f857bd706fcaa
ADmad <ADmad@users.noreply.github.com>
2018-10-27 11:50:06 +0530

    Simplify regex for extracting image URLs
    Fixes #182.

--- js/twitterFetcher.js
+++ js/twitterFetcher.js
@@ -88,9 +88,9 @@
 
   function extractImagesUrl(image_data) {
     if (image_data !== undefined && image_data.innerHTML.indexOf('data-image') >= 0) {
-      var data_src = image_data.innerHTML.match(/data-image=\"([A-z0-9]+:\/\/[A-z0-9]+\.[A-z0-9]+\.[A-z0-9]+\/[A-z0-9]+\/[A-z0-9\-]+)\"/ig);
+      var data_src = image_data.innerHTML.match(/data-image=\"([^"]+)\"/ig);
       for (var i = 0; i < data_src.length; i++) {
-        data_src[i] = data_src[i].match(/data-image=\"([A-z0-9]+:\/\/[A-z0-9]+\.[A-z0-9]+\.[A-z0-9]+\/[A-z0-9]+\/[A-z0-9\-]+)\"/i)[1];
+        data_src[i] = data_src[i].match(/data-image=\"([^"]+)\"/i)[1];
         data_src[i] = decodeURIComponent(data_src[i]) + '.jpg';
       }
       return data_src;
